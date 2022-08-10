1b52c74a9f46eb3874df1a0635e67acd1ee4bb14
Rebecca Xiong <rebeccaxiong111@gmail.com>
2022-08-09 20:08:25 -0400

    Added login section to home page

--- prototype/static/js/home.js
+++ prototype/static/js/home.js
@@ -51,3 +51,37 @@ for (var l = 0; l < testimDots.length; l++) {
 playSlide(currentSlide);
 
 }
+
+const container = document.querySelector(".container"), 
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signup = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+      pwShowHide.forEach(eyeIcon => {
+          eyeIcon.addEventListener("click", () => {
+              pwFields.forEach(pwField => {
+                  if(pwField.type === "password") {
+                      pwField.type = "text";
+
+                      pwShowHide.forEach(icon => {
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                      })
+                    }else {
+                        pwField.type = "password";
+
+                        pwShowHide.forEach(icon => {
+                            icon.classList.replace("uil-eye", "uil-eye-slash");
+                        })
+                    }
+              })
+          })
+      })
+
+      signup.addEventListener("click", () => {
+          container.classList.add("active");
+      })
+
+      login.addEventListener("click", () => {
+        container.classList.remove("active");
+    })
