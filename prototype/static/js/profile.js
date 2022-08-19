8d13f3af62e228f096d26beaacaae3b3478edb32
Rebecca Xiong <rebeccaxiong111@gmail.com>
2022-08-19 00:05:28 -0400

    Making user info editable

---
+++ prototype/static/js/profile.js
@@ -0,0 +1,52 @@
+var edit = $('.pencil');
+// var userChange = $('.userChange') //input for user
+var username = $('.username')
+// var email_input = $('.email_input')
+var email = $('.email')
+// var password_input = $('.password_input')
+var password = $('.password')
+// var bio_input = $('.bio_input')
+var bio = $('.bio')
+var text_secondary= $('.text-secondary')
+// var user_input = $('.user_info_input')
+var save_button = $('.save')
+
+
+
+    edit.click(function(){
+        edit.hide()
+        save_button.show()
+
+
+        username.attr('contenteditable','true')
+        email.attr('contenteditable','true')
+        password.attr('contenteditable','true')
+        bio.attr('contenteditable','true')
+
+       
+    
+})
+    save_button.click(function(){
+        
+        // alert(username.text())
+        save_button.hide()
+        edit.show()
+
+        username.attr('contenteditable','false')
+        email.attr('contenteditable','false')
+        password.attr('contenteditable','false')
+        bio.attr('contenteditable','false')
+    
+        // text_secondary.show()
+        // userChange.hide()
+        // email_input.hide()
+        // password_input.hide()
+        // bio_input.hide()
+        // save_button.hide()
+      
+    })
+
+  
+
+
+
